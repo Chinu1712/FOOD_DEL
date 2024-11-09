7f7d759282f04f986246341f80fa633d5e7344ac
Chinmay Gandhewar <chinmaygandhewar@gmail.com>
2024-11-09 19:06:39 +0530

    commit 1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here!</h2>
+        <p>Choose from a diverse menu featuring an array of delicious dishes from around the world, each crafted with fresh ingredients and authentic flavors. Whether you're craving spicy, savory, or sweet, our selections offer something special for every taste and occasion!</p>
+
+        <button>View Menu</button>   
+      </div>
+    </div>
+  )
+}
+
+export default Header
